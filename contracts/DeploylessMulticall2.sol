0ea4025a36c95ae390cdfa84180a3467fa6903ef
Timur Badretdinov <destinerx@gmail.com>
2021-12-06 08:02:09 +0300

    Fix Multicall2 for a single call query

--- contracts/DeploylessMulticall2.sol
+++ contracts/DeploylessMulticall2.sol
@@ -29,27 +29,30 @@ contract DeploylessMulticall2 {
             // returnData.size = memory.size - returnData.pointer - 2 *  32 * returnData.length + 32
             let size := add(sub(sub(mload(0x40), returnData), mul(0x40, mload(returnData))), 0x20)
             let start := sub(sub(mload(0x40), size), 0x40)
-            
+
             // Fix pointers
-            for
-                { let index := 0 }
-                lt(index, mload(returnData))
-                { index := add(index, 1) }
+            if gt(mload(returnData), 1)
             {
-                let oldPointer := add(add(returnData, 0x20), mul(0x20, index))
-                let pointer := add(start, add(0x40, mul(0x20, index)))
-                if iszero(index)
+                for
+                    { let index := 0 }
+                    lt(index, mload(returnData))
+                    { index := add(index, 1) }
                 {
-                    mstore(pointer, mul(0x20, mload(returnData)))
-                }
-                if gt(index, 0)
-                {
-                    let length := sub(
-                        mload(add(mload(oldPointer), 0x20)),
-                        mload(add(mload(sub(oldPointer, 0x20)), 0x20))
-                    )
-                    let value := add(mload(sub(pointer, 0x20)), length)
-                    mstore(pointer, value)
+                    let oldPointer := add(add(returnData, 0x20), mul(0x20, index))
+                    let pointer := add(start, add(0x40, mul(0x20, index)))
+                    if iszero(index)
+                    {
+                        mstore(pointer, mul(0x20, mload(returnData)))
+                    }
+                    if gt(index, 0)
+                    {
+                        let length := sub(
+                            mload(add(mload(oldPointer), 0x20)),
+                            mload(add(mload(sub(oldPointer, 0x20)), 0x20))
+                        )
+                        let value := add(mload(sub(pointer, 0x20)), length)
+                        mstore(pointer, value)
+                    }
                 }
             }
             
@@ -71,6 +74,14 @@ contract DeploylessMulticall2 {
             {
                 let oldPointer := add(add(returnData, 0x20), mul(0x20, index))
                 mstore(sub(mload(add(mload(oldPointer), 0x20)), 0x20), 0x40)
+            }
+
+            // Fix pointers (single element)
+            if eq(mload(returnData), 1)
+            {
+                let oldPointer := add(returnData, 0x20)
+                let pointer := add(start, 0x40)
+                mstore(pointer, mul(0x20, mload(returnData)))
             }
             
             // Write header
